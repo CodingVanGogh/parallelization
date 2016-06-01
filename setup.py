45ab5da8a41bd722a50e1ef2a13ee22303d8c1b3
lohithbr <brlohith@gmail.com>
2016-06-01 11:31:00 +0530

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='plmap',
-      version='1.0',
+      version='1.0.1',
       description='Python Package for accomplishing tasks parallely using multi-threading and multi-processing',
       author='Lohith B R',
       packages=['plmap'],
