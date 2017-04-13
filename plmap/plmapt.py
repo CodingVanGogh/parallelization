066ffbbc171ce20ba4764a06e69fba0c7d3a7a15
lohithbr <brlohith@gmail.com>
2017-04-13 12:25:20 +0530

    plmapt without sort

--- plmap/plmapt.py
+++ plmap/plmapt.py
@@ -1,47 +1,44 @@
-#!/usr/bin/python
-
-# This is a generic module for parallelization ( using Threads )
-# 
-# 
-# 
-# How to use This ? 
-#   Create a function that can do what you want to achieve with 1 parameter or 1 set of parameters
-#   pass this function and the set of arguments to the plmapt function
-#   plmapt will take care of parallelization for you 
-#   It will return lists of errros and outputs for the corresponding inputs
-# Eg : 
-
-#   Let say you want to add these pairs of numbers. 
-
-#   inputs = [
-#   [3, 5],
-#   [8, 9] ,
-#   [11, 12],
-#   [15, 16],
-#   ]
-
-#   step 1) define the function for a single set of inputs
-#   def add ( a, b ) :
-#       return a + b 
-
-#   step 2) Call the plmapt function 
-#   error, output = plmapt(add, inputs , [], 4, default_output=None)
-#   print output 
-
-# The output will look like this : 
-# [8, 17, 23, 31]
+#!/usr/bin/env python
+"""
+This is a generic module for parallelization ( using Threads )
 
 
+How to use This ? 
+  Create a function that can do what you want to achieve with 1 parameter or 1 set of parameters
+  pass this function and the set of arguments to the plmapt function
+  plmapt will take care of parallelization for you 
+  It will return lists of errros and outputs for the corresponding inputs
+Eg : 
 
+  Let say you want to add these pairs of numbers. 
+
+  inputs = [
+  [3, 5],
+  [8, 9] ,
+  [11, 12],
+  [15, 16],
+  ]
+
+  step 1) define the function for a single set of inputs
+  def add ( a, b ) :
+      return a + b 
+
+  step 2) Call the plmapt function 
+  error, output = plmapt(add, inputs , [], 4, default_output=None)
+  print output 
+
+The output will look like this : 
+[8, 17, 23, 31]
+"""
+
+
+from __future__ import division
 from threading import Thread
 from Queue import Queue
-from pprint import pprint
-import datetime
 import sys
 import time
-import csv
-import types
 
+from pprint import pprint
 
 PROGRESS_BAR_POLL_TIME = 1
 PROGRESS_BAR_LENGTH = 20
@@ -51,21 +48,19 @@ class Worker(Thread):
     """ Worker thread is the thread that actually executes the function with the given arguments
     """
 
-    def __init__(self, thread_id, input_queue, output_queue, default_output=None, progress_details={}, progress_bar_thread=False):
+    def __init__(self, thread_id, input_queue, default_output=None, progress_details={}, progress_bar_thread=False):
         super(Worker, self).__init__(name='%s' %(thread_id))
         self.input_queue = input_queue
-        self.output_queue = output_queue
         self.default_output = default_output
         self.progress_details = progress_details
         self.progress_bar_thread = progress_bar_thread
-        self.progress_details = progress_details
 
 
     def generate_loading_string(self, completed_tasks, total_tasks):
         """  <percentage completed>% [< -- based on percentage completion>] Completed/Total
         """
         try:
-            fraction_completed = ( completed_tasks * 1.0 / total_tasks) 
+            fraction_completed = ( completed_tasks / total_tasks) 
         except:
             fraction_completed = 1 # To avoid division by Zero
 
@@ -82,7 +77,6 @@ class Worker(Thread):
         return loading_string
 
 
-
     def display_progress_bar(self):
         """ 50%[---------->          ]5/10 
         """
@@ -96,7 +90,6 @@ class Worker(Thread):
             completed_tasks = self.progress_details['completed_tasks']
             total_tasks = self.progress_details['total_tasks']
 
-            
             print self.generate_loading_string(completed_tasks, total_tasks)
             sys.stdout.write("\033[F")
 
@@ -106,11 +99,9 @@ class Worker(Thread):
         print self.generate_loading_string(completed_tasks, total_tasks)
         
 
-
     # run method will be called when the thread is started using thread.start() , Hence overriding run method
     def run(self):
-        """ Pick up the task from the queue and execute it 
-            
+        """ Pick up the task from the queue and execute it  
         """ 
         if not self.progress_bar_thread:
             while True:
@@ -119,13 +110,15 @@ class Worker(Thread):
                 except:
                     break
 
-                task_id, func, args, kwargs = task
+                func, args, kwargs, error_loc, output_loc = task
                 
                 try:
                     output = func(*args, **kwargs)
-                    self.output_queue.put({ 'task_id' : task_id, 'error_code' : '0', 'error_desc' : 'None', 'output' : output})
+                    error_loc.append(0) # Indicates Success
+                    output_loc.append(output) 
                 except Exception, details:
-                    self.output_queue.put({ 'task_id' : task_id, 'error_code' : '1', 'error_desc' : details, 'output' : self.default_output})
+                    error_loc.append(1) # Indicates Failure
+                    output_loc.append(self.default_output) 
                 finally:
                     self.progress_details['completed_tasks'] += 1
         else:
@@ -139,9 +132,6 @@ def plmapt(func, args=[], kwargs=[], threads=10, default_output=None, sort_outpu
         Sorting the output according to the input order is optional (<sort_output>)
         Displaying the Progress bar is optional
     """
-    input_queue = Queue()
-    output_queue = Queue()
-
     bigger_array = args if len(args) > len(kwargs) else kwargs 
     big_len = len(bigger_array)
 
@@ -150,22 +140,26 @@ def plmapt(func, args=[], kwargs=[], threads=10, default_output=None, sort_outpu
 
     progress_details = {'total_tasks' : big_len , 'completed_tasks' : 0}
 
+    input_queue = Queue()
+    output_errors = [[] for _ in xrange(big_len)]
+    output_values = [[] for _ in xrange(big_len)]
+
     # Load the input_queue  ( big_len = the number of tasks )
-    for _ in xrange(big_len):
-        task = ( _, func, ag(args, _, ()), ag(kwargs, _, {}))
+    for i in xrange(big_len):
+        task = [func, ag(args, i, ()), ag(kwargs, i, {}), output_errors[i], output_values[i]]
         input_queue.put(task)
 
     # Create workers and start them
     list_of_workers = []
     for i in xrange(min(threads, big_len)):
-        worker = Worker(i, input_queue, output_queue, default_output, progress_details)
+        worker = Worker(i, input_queue, default_output, progress_details)
         worker.setDaemon(True)
         list_of_workers.append(worker)
         worker.start()
 
     # If the progress bar needs to be displayed
     if progress_bar:
-        progress_worker = Worker("-1", input_queue, output_queue, default_output, progress_details, True)
+        progress_worker = Worker("-1", input_queue, default_output, progress_details, True)
         progress_worker.setDaemon(True)
         list_of_workers.append(progress_worker)
         progress_worker.start()
@@ -174,18 +168,7 @@ def plmapt(func, args=[], kwargs=[], threads=10, default_output=None, sort_outpu
     for _ in list_of_workers:
         _.join()
 
-    # get the outputs in an array
-    output_dicts = []
-    for output_dict in xrange(big_len):
-        output_dicts.append(output_queue.get_nowait())
-
-    # Sort the output based on the id IF sorting is required
-    output_sorted_dicts =  sorted(output_dicts, key=lambda x : x['task_id']) if sort_output else output_dicts
-    
-    error_array = [ (_['error_code'], _['error_desc']) for _ in output_sorted_dicts ]
-    output_array = [ _['output'] for _ in output_sorted_dicts ]
-
-    return (error_array, output_array)
+    return output_errors, output_values
 
 
 
@@ -218,5 +201,6 @@ if __name__ == "__main__":
     print output 
     error, output = plmapt(add, inputs , [], 8, default_output=None, sort_output=False, progress_bar=False)
     print output
+    print error
 
 
